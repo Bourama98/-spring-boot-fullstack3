00afecee4ddeb5e7e2203a3c44144c18ae17399c
Bourama98 <mangara98@hotmail.com>
2025-06-24 13:37:25 -0700

    Added slack messaging functions and update the Dockerrun file with new database link.

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping("/ping")
     private PingPong getPingPong() {
-        return new PingPong("pong: Ping 8888cdd bb kk77755");
+        return new PingPong("pong: Ping 8888cdd bb kk7775588");
     }
 }
