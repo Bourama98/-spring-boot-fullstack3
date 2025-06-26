16da9ebe2c24a79071ec6cf5630ca1b768ada918
Development_PC_main <mangara98@hotmail.com>
2025-06-26 12:04:53 -0700

    test3

--- backend/src/main/java/com/amigoscode/config/WebMvcConfig.java
+++ backend/src/main/java/com/amigoscode/config/WebMvcConfig.java
@@ -1,11 +1,7 @@
 package com.amigoscode.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,49 +11,25 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Value("#{'${cors.allowed-origins}'.split(',')}")
+    @Value("#{ '*'.split(',') }")
     private List<String> allowedOrigins;
-
-    @Value("#{'${cors.allowed-methods}'.split(',')}")
+    @Value("#{ '${cors.allowed-methods}'.split(',')}")
     private List<String> allowedMethods;
-
-    @Value("#{'${cors.allowed-headers}'.split(',')}")
+    @Value("#{'*'.split(',')}")
     private List<String> allowedHeaders;
+    @Value("#{'*'.split(',')}")
+    private List<String> exposedHeaders;
 
-    @Value("#{'${cors.exposed-headers}'.split(',')}")
-    private List<String> expectedHeaders;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        CorsRegistration corsRegistration = registry.addMapping("/api/**");
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(allowedOrigins);
-        configuration.setAllowedMethods(allowedMethods);
-        configuration.setAllowedHeaders(allowedHeaders);
-        configuration.setExposedHeaders(expectedHeaders);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", configuration);
-        return (CorsConfigurationSource) source;
+        allowedOrigins.forEach(corsRegistration::allowedOrigins);
+        allowedMethods.forEach(corsRegistration::allowedMethods);
+        allowedHeaders.forEach(corsRegistration::allowedHeaders);
+        exposedHeaders.forEach(corsRegistration::exposedHeaders);
+
+
     }
-
-//    @Value("#{ '${cors.allowed-origins}'.split(',') }")
-//    private List<String> allowedOrigins;
-//    @Value("#{ '${cors.allowed-methods}'.split(',')}")
-//    private List<String> allowedMethods;
-//    @Value("#{'*'.split(',')}")
-//    private List<String> allowedHeaders;
-//    @Value("#{'*'.split(',')}")
-//    private List<String> exposedHeaders;
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        CorsRegistration corsRegistration = registry.addMapping("/api/**");
-//
-//        allowedOrigins.forEach(corsRegistration::allowedOrigins);
-//        allowedMethods.forEach(corsRegistration::allowedMethods);
-//        allowedHeaders.forEach(corsRegistration::allowedHeaders);
-//        exposedHeaders.forEach(corsRegistration::exposedHeaders);
-//
-//
-//    }
 
 }
