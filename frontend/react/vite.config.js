ce4e6061c52f3f20b2b4c0e894c7654b552f2a36
Development_PC_main <mangara98@hotmail.com>
2025-06-26 12:31:42 -0700

    test4

--- frontend/react/vite.config.js
+++ frontend/react/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts:[
-        'my-api2-env.eba-pd3scz3q.us-west-2.elasticbeanstalk.com'
+        'my-api2-env.eba-pd3scz3q.us-west-2.elasticbeanstalk.com',
+        'http://my-api-env.eba-pd3scz3q.us-west-2.elasticbeanstalk.com:8080',
         ]
   }
 })
