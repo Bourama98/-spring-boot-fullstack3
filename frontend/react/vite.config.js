0d73162a4903b41fa38f1b725049b657d8dde01a
Development_PC_main <mangara98@hotmail.com>
2025-06-26 03:28:06 -0700

    CD Depoy frontend branch with update and delete customer feature

--- frontend/react/vite.config.js
+++ frontend/react/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'my-api2-env.eba-psbhxpqq.us-west-2.elasticbeanstalk.com'
+    allowedHosts:'ALl'
   }
 })
