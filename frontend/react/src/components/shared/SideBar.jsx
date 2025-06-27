6ff62543aac98562d29a009ee24d39da7e0f3bff
Development_PC_main <mangara98@hotmail.com>
2025-06-27 08:27:06 -0700

    Change link color to Yellow

--- frontend/react/src/components/shared/SideBar.jsx
+++ frontend/react/src/components/shared/SideBar.jsx
@@ -114,7 +114,7 @@ const NavItem = ({icon, children, ...rest}) => {
             role="group"
             cursor="pointer"
             _hover={{
-                bg: 'red.500',
+                bg: 'yellow.500',
                 color: 'white',
             }}
             {...rest}>
